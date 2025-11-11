bff3d74012e88b21111d06b3aa2f63f871095851
Thong Le <lminhthong1@gmail.com>
2025-11-11 23:31:05 +0700

    Refactor

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,7 +9,7 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
-from .restapis import get_request, analyze_review_sentiments
+from .restapis import get_request, analyze_review_sentiments, add_review  # noqa: F401
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
