9bcb043c92a67b005fbe7d1b1c2f2c06ef6f7cfb
Thong Le <lminhthong1@gmail.com>
2025-11-11 23:26:03 +0700

    Refactor

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,14 +31,14 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     (
-        'https://lminhthong1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-        'proxy.cognitiveclass.ai'
+        'https://lminhthong1-8000.theiadockernext'
+        '-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     )
 ]
 CSRF_TRUSTED_ORIGINS = [
     (
-        'https://lminhthong1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-        'proxy.cognitiveclass.ai'
+        'https://lminhthong1-8000.theiadockernext'
+        '-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     )
 ]
 
@@ -105,7 +105,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        ('django.contrib.auth.password_validation.'
+         'UserAttributeSimilarityValidator'),
     },
     {
         'NAME':
